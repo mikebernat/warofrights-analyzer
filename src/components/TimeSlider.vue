373cc61d7c577dea156516d19e8a88a02bd7394a
Mike Bernat <mike@mikebernat.com>
2025-10-21 11:39:42 -0400

    UX tweak to the time range slider

--- src/components/TimeSlider.vue
+++ src/components/TimeSlider.vue
@@ -1,24 +1,26 @@
 <template>
   <div class="time-slider-container">
-    <v-label class="time-range-label">
-      <v-icon size="small" class="mr-1">mdi-clock-outline</v-icon>
-      Time Range: {{ formatTime(localRange[0]) }} - {{ formatTime(localRange[1]) }}
-    </v-label>
-    <div class="slider-wrapper">
-      <v-range-slider
-        v-model="localRange"
-        :min="bounds[0]"
-        :max="bounds[1]"
-        :step="1"
-        color="primary"
-        track-color="grey"
-        thumb-label="always"
-        @update:model-value="updateRange"
-      >
-        <template v-slot:thumb-label="{ modelValue }">
-          {{ formatTime(modelValue) }}
-        </template>
-      </v-range-slider>
+    <div class="d-flex align-center">
+      <v-label class="time-range-label">
+        <v-icon size="small" class="mr-1">mdi-clock-outline</v-icon>
+        Time Range
+      </v-label>
+      <div class="slider-wrapper flex-grow-1">
+        <v-range-slider
+          v-model="localRange"
+          :min="bounds[0]"
+          :max="bounds[1]"
+          :step="1"
+          color="primary"
+          track-color="grey"
+          thumb-label="always"
+          @update:model-value="updateRange"
+        >
+          <template v-slot:thumb-label="{ modelValue }">
+            {{ formatTime(modelValue) }}
+          </template>
+        </v-range-slider>
+      </div>
     </div>
   </div>
 </template>
@@ -55,17 +57,19 @@ const formatTime = (seconds) => {
 <style scoped>
 .time-slider-container {
   padding-top: 10px;
-  padding-left: 40px;
+  padding-left: 20px;
   padding-right: 40px;
 }
 
 .time-range-label {
-  display: block;
-  margin-bottom: 18px;
   font-size: 14px;
+  white-space: nowrap;
+  margin-right: 16px;
+  min-width: 120px;
 }
 
 .slider-wrapper {
   padding-top: 20px;
+  margin-left: 16px;
 }
 </style>
